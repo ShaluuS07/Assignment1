c9d7f4d9cb5a5d5b40a5bff465cbb468c15f3e89
ShaluuS07 <shalinisaravanan.s@caratlane.com>
2023-01-25 11:46:55 +0530

    commit 5

---
+++ app/src/main/java/com/example/retrofitdemo/ApiInterface.kt
@@ -0,0 +1,13 @@
+package com.example.retrofitdemo
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+
+interface ApiInterface {
+
+    @GET(value = "posts")
+
+    fun getData(): Call<List<MyDataItem>>
+
+}
---
+++ app/src/main/java/com/example/retrofitdemo/MyDataItem.kt
@@ -0,0 +1,8 @@
+package com.example.retrofitdemo
+
+data class MyDataItem(
+    val body: String,
+    val id: Int,
+    val title: String,
+    val userId: Int
+)
